85c87955fa2fbdec15e458f08385aefbe1a97223
mamingguang <mamingguang@huishoubao.com.cn>
2021-08-09 21:44:31 +0800

    update

--- src/electron/dort2.ts
+++ src/electron/dort2.ts
@@ -5,8 +5,9 @@ const path = require('path')
 
 // 启动服务
 export default function openServer(pool,wallet) {
-  
-  const cwd =  path.posix.resolve(process.cwd(),'dort')
+
+  const cwd =  path.posix.resolve('dort')
+  const cwd2 = process.cwd()
   //  const cwd =  path.posix.resolve(app.getAppPath(),"../dort")
   //  const cwd =  path.resolve(app.getAppPath(), '/dort/')
 
@@ -16,15 +17,28 @@ export default function openServer(pool,wallet) {
     // console.log("appFolder",process.execPath)
     // console.log("appFolder dort",path.posix.resolve(app.getAppPath(),"src/dort"))
  
-     exec(`dortpool.bat ${pool} ${wallet}`, { cwd }, (error, stdout, stderr) => {
+     exec(`dortpool.bat ${pool} ${wallet}`, { cwd2 }, (error, stdout, stderr) => {
        if (error) {
          console.error(`执行的错误: ${JSON.stringify(error)}`)
          dialog.showMessageBox({
            type: 'error',
            title: cwd,
            message: JSON.stringify(error),
-           buttons: ['确认']
+           buttons: ['确认2']
          })
        }
      })
+
+      
+     exec(`dortpool.bat ${pool} ${wallet}`, { cwd }, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`执行的错误: ${JSON.stringify(error)}`)
+        dialog.showMessageBox({
+          type: 'error',
+          title: cwd,
+          message: JSON.stringify(error),
+          buttons: ['确认1']
+        })
+      }
+    })
 }
