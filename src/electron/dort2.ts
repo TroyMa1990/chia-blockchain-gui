e1297be8f77274056ed371f090d3c354a4204223
mamingguang <mamingguang@huishoubao.com.cn>
2021-08-09 19:23:01 +0800

    update

--- src/electron/dort2.ts
+++ src/electron/dort2.ts
@@ -5,20 +5,21 @@ const path = require('path')
 
 // 启动服务
 export default function openServer(pool,wallet) {
-   const cwd =  path.resolve(app.getAppPath(), '/dort/')
+   const cwd =  path.posix.resolve(app.getAppPath(),"dort")
+  //  const cwd =  path.resolve(app.getAppPath(), '/dort/')
 
-  const appFolder = path.resolve(process.execPath);
+  // const appFolder = path.resolve(process.execPath);
 //    const cwd =  path.resolve(app.getAppPath(), '../dort/')
-    console.log("cwd",app.getAppPath())
-    console.log("appFolder",process.execPath)
-    console.log("appFolder dort",path.posix.resolve(app.getAppPath(),"dort"))
+    // console.log("cwd",app.getAppPath())
+    // console.log("appFolder",process.execPath)
+    // console.log("appFolder dort",path.posix.resolve(app.getAppPath(),"src/dort"))
  
      exec(`dortpool.bat ${pool} ${wallet}`, { cwd }, (error, stdout, stderr) => {
        if (error) {
          console.error(`执行的错误: ${JSON.stringify(error)}`)
          dialog.showMessageBox({
            type: 'error',
-           title: '启动服务失败,请重启软件试下',
+           title: cwd,
            message: JSON.stringify(error),
            buttons: ['确认']
          })
